897621380e0449fd1da6f8b014d1fdeab5a28b1b
katy-yats <79374257+katy-yats@users.noreply.github.com>
2021-05-30 17:34:46 +0300

    Html code

---
+++ js/currency_converter.js
@@ -0,0 +1,14 @@
+async function convertCurrency() {
+    const startCurrency = 'USD';
+    const targetCurrency = document.querySelector('.currency-switch').value;
+    const response = await fetch('https://api.exchangerate-api.com/v4/latest/USD');
+    const rates = await response.json();
+    const rate = rates.rates[targetCurrency];
+    for (const product of products) {
+        product.convertedPrice1 = product.price1 * rate;
+        product.convertedPrice2 = product.price2 * rate;
+        product.convertedPrice3 = product.price3 * rate;
+        product.convertedPrice4 = product.price4 * rate;
+        products.convertedCurrency = targetCurrency;
+    }
+}
